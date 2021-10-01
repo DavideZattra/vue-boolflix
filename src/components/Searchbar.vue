4c1322e3a5556eb339ca7b6d6946fbdf56feea39
DavideZattra <d.zattra@gmail.com>
2021-10-01 14:56:28 +0200

    initialized vue-boolflix

---
+++ src/components/Searchbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="container">
+      <section class="row">
+          <div class="col-12"></div>
+          <div class="col-12"></div>
+      </section>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
